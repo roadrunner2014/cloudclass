ec5847c21440b92e2fdf1b3e0df841374e53e4e3
roadrunner2014 <jmc@crydermanenterprises.com>
2018-11-19 21:17:44 -0800

    dotProduct with scatterv for HW3

--- dotProductParallelscat.py
+++ dotProductParallelscat.py
@@ -17,50 +17,61 @@ n = int(sys.argv[1]) #length of vectors
 
 #arbitrary example vectors, generated to be evenly divided by the
 #number of processes for convenience
-x = numpy.linspace(0,100,n) if comm.rank == 0 else None
-y = numpy.linspace(20,300,n) if comm.rank == 0 else None
+# send data vectors
+x = numpy.linspace(0,100,size*n) if comm.rank == 0 else None
+y = numpy.linspace(20,300,size*n) if comm.rank == 0 else None
+
+# Calculation of counts and displacements for Scatterv
+counts = numpy.empty([size], dtype=int)
+dspls = numpy.empty([size], dtype=int)
+countstep = n / size
+countstepextra = n % size
+finalcountstep = countstep + countstepextra
+sum = 0            #Sum of counts. Used to calculate displacements
+
+for i in range(0,size,1):
+    counts[i] = countstep
+    dspls[i] = sum
+    sum += 1
+
 
 #initialize as numpy arrays
 dot = numpy.array([0.])
-local_n = numpy.array([0])
+local_n = numpy.array([0], dtype=int)
 
-#test for conformability
-if rank == 0:
-	if (n != y.size):
-		print "vector length mismatch"
-		comm.Abort()
-
-	#currently, our program cannot handle sizes that are not evenly divided by the number of processors
-	if(n % size != 0):
-		print "the number of processors must evenly divide n."
-		comm.Abort()
-	#length of each process's portion of the original vector
-	local_n = numpy.array([n/size])
+#length of each process's portion of the original vector
+local_n = numpy.array([n/size], dtype=int)
 
 #communicate local array size to all processes
 comm.Bcast(local_n, root=0)
 
-#initialize as numpy arrays
+#initialize as numpy arrays for receive data
 local_x = numpy.zeros(local_n)
 local_y = numpy.zeros(local_n)
 
 #divide up vectors
-comm.Scatter(x, local_x, root=0)
-comm.Scatter(y, local_y, root=0)
 # example of comm.Scatterv(sendbuf, recvbuf, int root=0) where sendbuf = [data, counts, displacements, type]
-#a_size = 4
-#counts = None
-#dspls = None
-#recvdata = numpy.empty(a_size,dtype=numpy.float64)
-#senddata = numpy.arange(100,dtype=numpy.float64)
 comm.Scatterv([x,counts,dspls,MPI.DOUBLE],local_x,root=0)
 comm.Scatterv([y,counts,dspls,MPI.DOUBLE],local_y,root=0)
 
+
 #local computation of dot product
 local_dot = numpy.array([numpy.dot(local_x, local_y)])
+print ("Process " + str(rank) + "Local dot calculation =", local_dot)
+
+#comm.Barrier()
+#sum the results of each
+sendbuf = [local_dot,counts[rank]]
+recvbuf = [dot,counts,dspls, MPI.DOUBLE]
+comm.Gatherv(sendbuf,recvbuf,root=0)
+print ("Process " + str(rank) + " Gather dot calculation =", dot)
 
 #sum the results of each
-comm.Gatherv(local_dot, dot, MPI.DOUBLE)
+comm.Reduce(local_dot, dot, op = MPI.SUM)
+#comm.Reduce(local_dot, dot, op = MPI.MAX(local_dot,dot))
 if (rank == 0):
-	print "The dot product is", dot[0], "computed in parallel"
-	print "and", numpy.dot(x,y), "computed serially"
+    print "The dot product is", dot[0], "computed in parallel"
+    print "and", numpy.dot(x,y), "computed serially"
+
+end = time.time()
+print "Time to compute = ", (end-start)
