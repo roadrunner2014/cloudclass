13483c34fe2008c5c9a8460f2152e8cd3637ff68
roadrunner2014 <jmc@crydermanenterprises.com>
2018-11-19 21:44:49 -0800

    files for matrix vector math

---
+++ matrixvectormultiplication.py
@@ -0,0 +1,44 @@
+#-------------- scattertest.py --------------------------
+# for correct performance, run unbuffered with 3 processes:
+# mpiexec -n 3 python26 scratch.py -u
+import numpy
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+A = numpy.array([[1.,2.,3.],[4.,5.,6.],[7.,8.,9.]])
+local_a = numpy.zeros(3)
+comm.Scatter(A,local_a)
+print "process", rank, "has", local_a
+
+if rank == 0:
+    x = numpy.linspace(0,100,11)
+else:
+    x = None
+
+if rank == 2:
+    xlocal = numpy.zeros(9)
+else:
+    xlocal = numpy.zeros(1)
+
+if rank ==0:
+    print "Scatter"
+
+counts = [1,1,9]
+comm.Scatterv([x,counts,(0,1,2),MPI.DOUBLE],xlocal)
+print ("process " + str(rank) + " has " +str(xlocal))
+
+comm.Barrier()
+if rank == 0:
+    print "Gather"
+    xGathered = numpy.zeros(11)
+else:
+    xGathered = None
+
+comm.Gatherv(xlocal,[xGathered,counts,(0,1,2),MPI.DOUBLE])
+print ("process " + str(rank) + " has " +str(xGathered))
+
+#comm.Reduce(xlocal,x, op = MPI.SUM)
+#print x[0]
